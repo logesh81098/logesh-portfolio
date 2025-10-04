9c850d5736c6c37397b3a7440190f0ea57cd1c5c
Logesh Shanmugavel <logesh81098@gmail.com>
2025-10-05 00:58:22 +0530

    Converted private bucket to public bucket

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -9,3 +9,14 @@ resource "aws_s3_bucket" "logesh-portfolio" {
     Project = "Logesh Portfolio"
   }
 }
+
+
+#Converting private bucket to public bucket 
+
+resource "aws_s3_bucket_public_access_block" "private-to-public" {
+  bucket = aws_s3_bucket.logesh-portfolio.id
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
